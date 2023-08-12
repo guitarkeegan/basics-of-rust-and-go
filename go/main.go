479e133f903119388a834f61d99f1ec0337838b0
guitarkeegan <keegananglim@gmail.com>
2023-08-12 01:21:46 -0700

    variables

--- go/main.go
+++ go/main.go
@@ -1,5 +1,14 @@
 package main
 
+// global variable
+var name = "Keegan"
+
 func main() {
-	print("Hello, World!")
+	// function scope variable
+	favoriteMusician := "Brad Mehldau"
+	approximateAge := 25 + 23
+	const info = "gmoat"
+
+	// no formatting and % at the end
+	print(favoriteMusician, approximateAge, info)
 }
