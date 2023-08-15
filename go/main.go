0196598595603461083ab807417129509bf1ef5c
guitarkeegan <keegananglim@gmail.com>
2023-08-14 17:21:56 -0700

    add mutible references as arguments

--- go/main.go
+++ go/main.go
@@ -2,40 +2,14 @@ package main
 
 import "fmt"
 
-func GetColors(color string) (string, int) {
-	if color == "blue" {
-		return "blue", 1
-	} else {
-		return "red", 2
-	}
+// * is a pointer to the value, otherwise it would just be a copy
+func turnClockBack(age *int) {
+	*age -= 1
 }
 
 func main() {
-	x := 4
-	y := 5
-	// Add is available because of the package main
-	ans := Add(x, y)
-	// use fmt to make the console log universal
-	fmt.Println(ans)
-
-	// array
-	var Names [1]string
-	Names[0] = "Bill"
-	fmt.Println(Names[0])
-
-	// dynamic list
-	var Numbers = []int{1, 2, 3}
-	Numbers = append(Numbers, 4)
-	fmt.Println(Numbers)
-
-	// map
-	ids := map[string]int{"joe": 23, "bob": 22}
-	fmt.Println(ids)
-
-	// we can return more than one value with a function
-	// use the _ if a value being returned will not be used
-	// variable that are actually values, and they are copies
-	newColor, _ := GetColors("orange")
-	fmt.Println(newColor)
-
+	age := 40
+	// & will give the address to the value in memory
+	turnClockBack(&age)
+	fmt.Printf("I am now %d!\n", age)
 }
