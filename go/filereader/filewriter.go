20cfc0b36f0489579b3aef4ce8cbd4ba82a05dd2
guitarkeegan <keegananglim@gmail.com>
2023-09-09 22:56:29 -0700

    go wackyness

--- go/filereader/filewriter.go
+++
@@ -1,7 +0,0 @@
-package fileworker
-
-import "os"
-
-func WriteToFile(fileName string, content string) error {
-	return os.WriteFile(fileName, []byte(content), 0644)
-}
--- go/main.go
+++ go/main.go
@@ -1,21 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	fileworker "reading.com/files/filereader"
-)
-
 func main() {
-	rootPath, _ := os.Getwd()
-	filePath := rootPath + "/data/text.txt"
-	data, err := fileworker.ReadTextFile(filePath)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("here is the data: %s", data)
-		newContent := fmt.Sprintf("Original: %v\n Double Original: %v\n", data, data)
-		fileworker.WriteToFile(filePath+".ouput.txt", newContent)
-	}
 }
